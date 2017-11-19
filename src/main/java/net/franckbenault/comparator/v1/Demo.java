cc0d7ed906df4b09d7839bd11b13c222669c23e0
franck-benault <franck-benault@orange.fr>
2017-11-19 20:22:12 +0100

    add logging in first example

--- src/main/java/net/franckbenault/comparator/v1/Demo.java
+++ src/main/java/net/franckbenault/comparator/v1/Demo.java
@@ -1,6 +1,7 @@
 package net.franckbenault.comparator.v1;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import static java.util.stream.Collectors.toCollection;
@@ -11,8 +12,18 @@ public class Demo {
 		List<Integer> list = new Random(209).ints(32L).boxed().collect(toCollection(ArrayList::new));
 		
 		//error very difficult to debug...
-		list.sort( (a, b) -> a-b);
+		//list.sort( (a, b) -> a-b);
+		list.sort(logging((a,b)-> a-b));
 		
 		System.out.println(list);
+	}
+	
+	private static Comparator<Integer> logging(Comparator<Integer> c) {
+		
+		return (a,b) -> {
+			int r = c.compare(a,b);
+			System.out.printf("a= %d, b=%d, r=%d \n",a,b,r); 
+			return r;
+		};
 	}
 }
